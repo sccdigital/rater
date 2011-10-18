73419c35a072560b836eac34ab1dd80ae649fd77
Digital Traffic <digitaltraffic@digitraf2.local>
2011-10-18 10:59:15 -0500

    changed rater route:

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   namespace :rater do
-    match 'ratings/:rateable_type/:rateable_id/:stars' => 'ratings#create', :as => :create_rating
+    match 'rate/:rateable_type/:rateable_id/:stars' => 'ratings#create', :as => :create_rating
 	end
 end
